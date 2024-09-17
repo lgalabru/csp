0a26bf6a04c47893c69d34bb6f5e9936a2f622e1
Rafael Cardenas <rafael@rafaelcr.com>
2024-09-16 21:43:14 -0600

    test: transfers channel

--- components/ordhook-core/src/core/pipeline/processors/transfers_recomputing.rs
+++ components/ordhook-core/src/core/pipeline/processors/transfers_recomputing.rs
@@ -107,3 +107,70 @@ pub fn start_transfers_recomputing_processor(
         thread_handle: handle,
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{thread, time::Duration};
+
+    use chainhook_sdk::{types::BitcoinBlockData, utils::Context};
+    use crossbeam_channel::unbounded;
+
+    use crate::{
+        config::Config,
+        core::{
+            pipeline::PostProcessorCommand,
+            test_builders::{TestBlockBuilder, TestTransactionBuilder},
+        },
+        db::{
+            blocks::open_blocks_db_with_retry, cursor::BlockBytesCursor, drop_all_dbs,
+            initialize_sqlite_dbs,
+        },
+    };
+
+    use super::start_transfers_recomputing_processor;
+
+    #[test]
+    fn transfers_recompute_via_processor() {
+        let ctx = Context::empty();
+        let config = Config::test_default();
+        {
+            drop_all_dbs(&config);
+            let _ = initialize_sqlite_dbs(&config, &ctx);
+            let _ = open_blocks_db_with_retry(true, &config, &ctx);
+        }
+
+        let (block_tx, block_rx) = unbounded::<BitcoinBlockData>();
+        let controller = start_transfers_recomputing_processor(&config, &ctx, Some(block_tx));
+
+        let c0 = controller.commands_tx.clone();
+        thread::spawn(move || {
+            let block0 = TestBlockBuilder::new()
+                .hash(
+                    "0x00000000000000000001b228f9faca9e7d11fcecff9d463bd05546ff0aa4651a"
+                        .to_string(),
+                )
+                .height(849999)
+                .add_transaction(
+                    TestTransactionBuilder::new()
+                        .hash(
+                            "0xa321c61c83563a377f82ef59301f2527079f6bda7c2d04f9f5954c873f42e8ac"
+                                .to_string(),
+                        )
+                        .build(),
+                )
+                .build();
+            thread::sleep(Duration::from_millis(50));
+            let _ = c0.send(PostProcessorCommand::ProcessBlocks(
+                vec![(
+                    849999,
+                    BlockBytesCursor::from_standardized_block(&block0).unwrap(),
+                )],
+                vec![block0],
+            ));
+        });
+        let _ = block_rx.recv().unwrap();
+
+        // Close channel.
+        let _ = controller.commands_tx.send(PostProcessorCommand::Terminate);
+    }
+}
